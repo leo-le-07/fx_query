dec8688f89d8ac18d703a3494c4186359252717c
Leo <coder.leo.le@gmail.com>
2019-01-04 14:02:29 +0700

    finish

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
 var express = require('express')
+const logger = require('morgan');
 var app = express()
 var bodyParser = require('body-parser')
 const axios = require('axios')
 
 const botToken = '689990809:AAFeUxR6wlnbH7YgIlhLeNeoDR0iHILMmlw';
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-) // for parsing application/x-www-form-urlencoded
+app.use(logger('dev'));
 
-//This is the route the API will call
-app.post('/new-message', function(req, res) {
-  const { message } = req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-  //Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
-  if (!message || message.text.toLowerCase().indexOf('marco') < 0) {
-    // In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-    return res.end()
-  }
-
-  // If we've gotten this far, it means that we have received a message containing the word "marco".
-  // Respond by hitting the telegram bot API and responding to the approprite chat_id with the word "Polo!!"
-  // Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-  axios
-    .post(
-      "https://api.telegram.org/bot" + botToken + "/sendMessage",
-      {
-        chat_id: message.chat.id,
-        text: 'Polo!!'
-      }
-    )
-    .then(response => {
-      // We get here if the message was successfully posted
-      console.log('Message posted')
-      res.end('ok')
-    })
-    .catch(err => {
-      // ...and here if it was not
-      console.log('Error :', err)
-      res.end('Error :' + err)
-    })
+app.get('*', (req, res) => {
+  console.log('insideeeee')
+  return res.status(200).send({
+    message: 'Welcome to the beginning of nothingness.!',
+  });
 })
 
+app.post('/start_bot', async function(req, res) {
+  try {
+    const { message } = req.body
+
+    if (!message) return res.end();
+
+    const messageLower = message.text.toLowerCase();
+    console.log("message", messageLower);
+
+    const sendMessage = (content) => (
+      axios
+        .post(
+          "https://api.telegram.org/bot689990809:AAFeUxR6wlnbH7YgIlhLeNeoDR0iHILMmlw/sendMessage",
+          {
+            chat_id: message.chat.id,
+            text: content
+          }
+        )
+        .then(response => {
+          // We get here if the message was successfully posted
+          console.log('Message posted')
+          res.end('ok')
+        })
+        .catch(err => {
+          // ...and here if it was not
+          console.log('Error :', err)
+          res.end('Error :' + err)
+        })
+    );
+    const getPair = (query) => {
+      const pair = query.toUpperCase();
+      return pair;
+    };
+    const response = await axios.get("https://forex.1forge.com/1.0.1/quotes?pairs="+getPair(messageLower)+"&api_key=W5rK1e0lzK1FNLU75E8KbkPhMLq1zADu");
+    const { symbol, bid, ask, price } = response.data[0];
+    const content = symbol + " - price " + price + " ("+bid+"-"+ask+")";
+    return sendMessage(content);
+  } catch(e) {
+    const errorMessage = "Loi roi!!!."
+    console.log('=== ERROR: ', e);
+    return sendMessage(errorMessage);
+  }
+});
+
 // Finally, start our server
-app.listen(3000, function() {
+app.listen(3333, function() {
   console.log('Telegram app listening on port 3000!')
 })
